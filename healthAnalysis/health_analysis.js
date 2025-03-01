4da7ee7c987956731c7e2c9d5efbf3e66dc57d1d
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-03-01 09:44:57 -0500

    finalized search functionality

--- healthAnalysis/health_analysis.js
+++ healthAnalysis/health_analysis.js
@@ -76,13 +76,28 @@ function searchCondition() {
         .then(response => response.json())
         .then(data => {
             const condition = data.conditions.find(item => item.name.toLowerCase() === input)
+
             if (condition) {
+            
                 const symptoms = condition.symptoms.join(', ')
                 const prevention = condition.prevention.join(', ')
-                const treatment = condititon.treatment
+                const treatment = condition.treatment
 
-                resultDiv.innerHTML += 
+                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+                resultDiv.innerHTML += `<img src="thyroid.jpeg" alt="hjh">`;
+                resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+                resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+                resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            } 
+            else {
+              resultDiv.innerHTML = 'Condition not found.';
             }
         })
 
+        .catch(error => {
+            console.error('Error:' , error)
+            resultDiv.innerHTML = 'An error has occured while fetching the data. '
+        })
 }
+
+btnSearch.addEventListener('click', searchCondition)
